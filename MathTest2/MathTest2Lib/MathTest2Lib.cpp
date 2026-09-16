1964208711d8e30101ece42392b599439b790c25
Александр Смирнов <alekssmirnov610@gmail.com>
2026-09-17 01:21:47 +0300

    Project template added

---
+++ MathTest2/MathTest2Lib/MathTest2Lib.cpp
@@ -0,0 +1,10 @@
+﻿// MathTest2Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnMathTest2Lib()
+{
+}
